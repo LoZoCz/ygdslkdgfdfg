653d7c020acc797ffe5288a56a2b334890b87fb3
LoZo <87641267+LoZoCz@users.noreply.github.com>
2022-12-11 02:06:32 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,57 @@
 const zdj = document.querySelectorAll('img');
 
-console.log(zdj[1]);
+for(let i = 0; i < zdj.length; i++){
 
-for(let i = 0; i < 4; i++){
+    zdj[i].addEventListener('mouseover', () => {
+        var src = zdj[i].src;
+        // console.log(src);
 
-    zdj[i].addEventListener('mouseenter', () => {
-        console.log(zdj[i]);
+        var len = src.length;
+        // console.log(len);
+
+        var slc1 = src.slice(22, len - 4);
+        // console.log(slc1);
+
+        var slc2 = src.slice(len-4, len);
+        // console.log(slc2);
+
+        var finSrc = slc1 + '-szary' + slc2;
+        // console.log(finSrc);
+
+        zdj[i].animate(animShow, animTime);
+        zdj[i].src = finSrc;
     })
 
+    zdj[i].addEventListener('mouseout', () => {
+        var src = zdj[i].src;
+        // console.log(src);
+
+        var len = src.length;
+        // console.log(len);
+
+        var slc1 = src.slice(22, len - 10);
+        // console.log(slc1);
+
+        var slc2 = src.slice(len-4, len);
+        // console.log(slc2);
+
+        var finSrc = slc1 + slc2;
+        // console.log(finSrc);
+
+        zdj[i].animate(animShow, animTime);
+        zdj[i].src = finSrc;
+    })
+
+    const animShow = [
+        { opacity: '1'},
+        { opacity: '0.5'},
+        { opacity: '1'}
+      ];
+      
+      const animTime = {
+        duration: 700,
+        iterations: 1,
+      }
+
 }
+
