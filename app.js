232c12a3b06b0525dfb0781d9de1716d7aa0ecb2
LoZo <87641267+LoZoCz@users.noreply.github.com>
2022-12-16 00:07:19 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const zdj = document.querySelectorAll('img');
+const zdjLrg = document.querySelector('.zdj-lrg');
 
 for(let i = 0; i < zdj.length; i++){
 
@@ -12,7 +13,7 @@ for(let i = 0; i < zdj.length; i++){
         var slc1 = src.slice(22, len - 4);
         // console.log(slc1);
 
-        var slc2 = src.slice(len-4, len);
+        var slc2 = src.slice(len - 4, len);
         // console.log(slc2);
 
         var finSrc = slc1 + '-szary' + slc2;
@@ -32,7 +33,7 @@ for(let i = 0; i < zdj.length; i++){
         var slc1 = src.slice(22, len - 10);
         // console.log(slc1);
 
-        var slc2 = src.slice(len-4, len);
+        var slc2 = src.slice(len - 4, len);
         // console.log(slc2);
 
         var finSrc = slc1 + slc2;
@@ -54,4 +55,24 @@ for(let i = 0; i < zdj.length; i++){
       }
 
 }
+for(let a = 0; a < zdj.length-1; a++)
 
+    zdj[a].addEventListener('click', () => {
+      var src = zdj[a].src;
+      // console.log(src);
+
+      var len = src.length;
+      // console.log(len);
+
+      var slc1 = src.slice(22, len - 10);
+      // console.log(slc1);
+
+      var slc2 = src.slice(len - 4, len);
+      // console.log(slc2);
+
+      var finSrc = slc1 + slc2;
+      // console.log(finSrc);
+
+      zdjLrg.setAttribute('src', finSrc)
+      // console.log(zdj[a])
+})
